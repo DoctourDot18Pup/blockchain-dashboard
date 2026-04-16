5e19a2b569b01c84bf1726d78fe421abddf4cb23
DoctourDot18Pup <21031430@itcelaya.edu.mx>
2026-04-16 14:47:45 -0600

    fix: mapear campos camelCase a snake_case en postTransaction

--- src/services/api.js
+++ src/services/api.js
@@ -10,7 +10,15 @@ export const getNodes    = ()          => api.get('/nodes')
 export const mine        = ()          => api.post('/mine')
 export const resolve     = ()          => api.get('/nodes/resolve')
 
-export const postTransaction = (data)  => api.post('/transactions', data)
+export const postTransaction = (data) => api.post("/transactions", {
+  persona_id:      data.personaId,
+  institucion_id:  data.institucionId,
+  programa_id:     data.programaId,
+  titulo_obtenido: data.tituloObtenido,
+  fecha_fin:       data.fechaFin,
+  numero_cedula:   data.numeroCedula,
+  firmado_por:     data.firmadoPor,
+})
 export const registerNodes   = (lista) =>
   Promise.all(lista.map(url => api.post('/nodes/register', { url })))
 
